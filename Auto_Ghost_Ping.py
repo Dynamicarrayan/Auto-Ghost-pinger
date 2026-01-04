9823a73f9b2862f7c753a6c6e493cf383ceda59f
Dynamicarrayan <arrayannayaga@gmail.com>
2026-01-05 03:40:03 +0700

    Add files via upload

---
+++ Auto_Ghost_Ping.py
@@ -0,0 +1,66 @@
+# PROGRAM: Autoghostpinger
+# This program ghost ping repeatedly when activated with F4 and stops with F2
+
+# DEKLARASI - Library
+import keyboard 
+import pyautogui
+import time
+
+# DEKLARASI - Variable
+active = False
+deactive = False
+
+# DEKLARASI - Subprogram
+def toggle_on():
+    global active
+    active = True
+    print(f"Activated! Will ghost ping {ping} repeatedly.")
+
+def toggle_off():
+    global active
+    active = False
+    print("Stop!")
+
+def addname():
+    global active
+    active = True
+    global ping
+    ping = input("please input the name of ur target for example @someone : ")
+
+
+#algoritma
+if __name__ == "__main__":
+
+    #input program
+    ping = input("please input the name of ur target for example @someone : ")
+    print("PRESS F11 first for full screen or else it didnt work")
+    print("Press F4 to activate, F2 to deactivate, F to rename.")
+    
+    # BIND KEYS
+    keyboard.add_hotkey('F4', toggle_on)
+    keyboard.add_hotkey('F2', toggle_off)
+    keyboard.add_hotkey("F", addname)
+
+    #proses program
+    try:
+        while True:
+            if active:
+                pyautogui.typewrite(f'{ping}\n')
+                time.sleep(0.1)
+                pyautogui.press('enter')
+                time.sleep(2)
+                pyautogui.press('up')
+                time.sleep(0.1)
+                pyautogui.hotkey('ctrl', 'a')
+                time.sleep(0.1)
+                pyautogui.press('backspace')
+                time.sleep(0.1)
+                pyautogui.press('enter')
+                time.sleep(0.1)
+                pyautogui.press('enter')
+                time.sleep(2)
+            else:
+                time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        print("Program stopped.")
